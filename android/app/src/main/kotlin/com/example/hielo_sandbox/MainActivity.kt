113e8889b5f20e10cd1ee49120e2013914d85ac6
suzanpradhan <sujanpradhan478@gmail.com>
2022-07-14 13:43:27 +0545

    Starter Project

---
+++ android/app/src/main/kotlin/com/example/hielo_sandbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hielo_sandbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/buttons/sample_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SampleButton extends StatefulWidget {
+  const SampleButton({Key? key}) : super(key: key);
+
+  @override
+  State<SampleButton> createState() => _SampleButtonState();
+}
+
+class _SampleButtonState extends State<SampleButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
